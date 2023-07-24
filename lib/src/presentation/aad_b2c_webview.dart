b7db477ee61984a13f111510805c63fbc178f64f
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2023-07-24 15:20:07 -0700

    Use latest version of webview_flutter

--- lib/src/presentation/aad_b2c_webview.dart
+++ lib/src/presentation/aad_b2c_webview.dart
@@ -6,6 +6,13 @@ import 'package:aad_b2c_webview/src/services/models/token.dart';
 import 'package:flutter/material.dart';
 import 'package:pkce/pkce.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+// #docregion platform_imports
+// Import for Android features.
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+// Import for iOS features.
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+// #enddocregion platform_imports
+
 import '../constants.dart';
 
 /// A widget that embeds the Azure AD B2C web view for authentication purposes.
@@ -56,6 +63,8 @@ class ADB2CEmbedWebViewState extends State<ADB2CEmbedWebView> {
   bool isLoading = true;
   bool showRedirect = false;
 
+  late final WebViewController _controller;
+
   @override
   void initState() {
     onRedirect = widget.onRedirect ??
@@ -63,9 +72,71 @@ class ADB2CEmbedWebViewState extends State<ADB2CEmbedWebView> {
           Navigator.of(context).pop();
         };
 
-    //Enable virtual display.
-    if (Platform.isAndroid) WebView.platform = AndroidWebView();
     super.initState();
+
+    // #docregion platform_features
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller =
+        WebViewController.fromPlatformCreationParams(params);
+    // #enddocregion platform_features
+
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (String url) {
+            if (mounted) {
+              setState(() {
+                isLoading = false;
+              });
+
+              final Uri response = Uri.dataFromString(url);
+              //Check that the user is past authentication and current URL is the redirect with the code.
+              onPageFinishedTasks(url, response);
+            }
+          },
+          onWebResourceError: (WebResourceError error) {
+            debugPrint('''
+Page resource error:
+  code: ${error.errorCode}
+  description: ${error.description}
+  errorType: ${error.errorType}
+  isForMainFrame: ${error.isForMainFrame}
+          ''');
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith(widget.redirectUrl)) {
+              debugPrint('blocking navigation to ${request.url}');
+              return NavigationDecision.prevent;
+            }
+
+            debugPrint('allowing navigation to ${request.url}');
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(getUserFlowUrl(
+          userFlow: "${widget.tenantBaseUrl}/${Constants.userFlowUrlEnding}")));
+
+    // #docregion platform_features
+    if (controller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+    // #enddocregion platform_features
+
+    _controller = controller;
   }
 
   /// Callback function for handling any token received.
@@ -153,31 +224,7 @@ class ADB2CEmbedWebViewState extends State<ADB2CEmbedWebView> {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          WebView(
-            key: _key,
-            debuggingEnabled: true,
-            initialUrl: getUserFlowUrl(
-                userFlow:
-                    "${widget.tenantBaseUrl}/${Constants.userFlowUrlEnding}"),
-            javascriptMode: JavascriptMode.unrestricted,
-            navigationDelegate: (navigation) {
-              if (navigation.url.startsWith(widget.redirectUrl)) {
-                return NavigationDecision.prevent;
-              }
-              return NavigationDecision.navigate;
-            },
-            onPageFinished: (String url) {
-              if (mounted) {
-                setState(() {
-                  isLoading = false;
-                });
-
-                final Uri response = Uri.dataFromString(url);
-                //Check that the user is past authentication and current URL is the redirect with the code.
-                onPageFinishedTasks(url, response);
-              }
-            },
-          ),
+          WebViewWidget(key: _key, controller: _controller),
           Visibility(
               visible: (isLoading || showRedirect),
               child: const Center(
